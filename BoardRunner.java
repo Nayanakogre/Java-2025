9d5bd2a3eb0641c17ecf7b8645c0acea2ca86208
Nayana-git23 <nayanakj.xworkz@gmail.com>
2025-02-03 13:30:48 +0530

    hello

---
+++ BoardRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.protectedkeyword.external;
+
+import com.xworkz.protectedkeyword.internal.Board;
+
+public class BoardRunner {
+	
+	public static void main(String[] args) {
+		
+		Board board=new Board("running",2000);
+		board.print();
+		
+	
+	}
+
+}
